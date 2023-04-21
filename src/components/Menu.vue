0cb88612596537004644ef44e5f8a5a06c076f06
António Camarão <camaraoacsn@gmail.com>
2023-04-21 11:32:06 +0100

    Created a Menu with buttons

---
+++ src/components/Menu.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="flex justify-center mt-8 gap-5">
+        <MenuButton :text="'List Users'" @click="listUsers" />
+        <MenuButton :text="'List Squads'" @click="listSquads" />
+        <MenuButton :text="'Create User'" @click="createUser" />
+        <MenuButton :text="'Create Squad'" @click="createSquad" />
+    </div>
+</template>
+
+<script>
+import MenuButton from './widgets/MenuButton.vue';
+
+export default {
+    components: {
+        MenuButton
+    },
+    methods: {
+        listUsers() {
+            this.$router.push({ name: "ListUsers" });
+        },
+        listSquads() {
+            this.$router.push({ name: "ListSquads" });
+        },
+        createUser() {
+            this.$router.push({ name: "CreateUser" });
+        },
+        createSquad() {
+            this.$router.push({ name: "CreateSquad" });
+        }
+    }
+}
+</script>
