affb6d8a1f4be53d63ebfae149ebd9d048fd0b8a
António Camarão <camaraoacsn@gmail.com>
2023-04-24 16:57:23 +0100

    Corrected Button CSS

--- src/components/Popups/ConfirmationDelete.vue
+++ src/components/Popups/ConfirmationDelete.vue
@@ -13,8 +13,8 @@
                 </div>
                 <div class="bg-white2 rounded-b-xl">
                     <div class="flex justify-center space-x-2 mb-6">
-                        <Button :text="'Delete'" class="text-red-600 hover:text-black2 before:bg-red-600 after:bg-red-600 py-0.5 px-3.5" @click="deleteUser(userToDelete)"/>
-                        <Button :text="'Cancel'" class="text-blue-600 hover:text-black2 before:bg-blue-600 after:bg-blue-600 py-0.5 px-4" @click="cancel"/>
+                        <Button :text="'Delete'" class="text-red-600 hover:text-white2 before:bg-red-600 after:bg-red-600 py-0.5 px-3.5" @click="deleteUser(userToDelete)"/>
+                        <Button :text="'Cancel'" class="text-blue-600 hover:text-white2 before:bg-blue-600 after:bg-blue-600 py-0.5 px-4" @click="cancel"/>
                     </div>
                 </div>
             </div>
