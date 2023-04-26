14760ebb744877edc38c3e8c3c2291f85d99e0d7
António Camarão <camaraoacsn@gmail.com>
2023-04-26 13:49:59 +0100

    List Squads shows all info and has all functions

--- src/components/ListSquads.vue
+++ src/components/ListSquads.vue
@@ -1,10 +1,12 @@
 <template>
     <Menu :title="'List Squads'"/>
-    <div class="grid grid-cols-3 mt-8 text-white2 mb-8">
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 2xl:grid-cols-3 text-white2 mt-8 mb-8 sm:gap-10">
         <div v-for="squad in getSquads" :key="squad.id" class="flex justify-center">
-            <Card :reference="squad.reference" :squadTitle="squad.squadName" :membersNames="squad.members"/>
+            <Card :squad="squad" @toggleEdit="toggleEditPopup" @toggleDelete="toggleDeletePopup(squad)"/>
         </div>
     </div>
+    <DeleteSquad v-if="deleteSquad" :squadToDelete="squadToDelete" @squadDeleted="deleteThisSquad"  @cancelSquad="toggleDeletePopup"/>
+    <EditSquad v-if="editSquad" :squadToEdit="squadToEdit" @squadEdited="editThisSquad" @cancelEditSquad="toggleEditPopup"/>
 </template>
 
 <script>
@@ -12,18 +14,46 @@ import Menu from './Menu.vue';
 import Card from './widgets/Card.vue';
 import { mapState } from 'pinia';
 import { squadStore } from '../stores/squadStore';
+import DeleteSquad from './Popups/DeleteSquad.vue';
+import EditSquad from './Popups/EditSquad.vue';
 
 export default {
     components: {
         Card,
-        Menu
+        Menu,
+        DeleteSquad,
+        EditSquad
     },
     setup() {
         const squadStoreT = squadStore()
         return { squadStoreT }
     },
     computed: {
-        ...mapState(squadStore, ['getSquads', 'getCount']),
+        ...mapState(squadStore, ['getSquads']),
     },
+    data(){
+        return {
+            deleteSquad: false,
+            editSquad: false,
+        }
+    },
+    methods: {
+        toggleEditPopup(squad) {
+            this.editSquad = !this.editSquad;
+            this.squadToEdit = squad;
+        },
+        toggleDeletePopup(squad) {
+            this.deleteSquad = !this.deleteSquad;
+            this.squadToDelete = squad;
+        },
+        editThisSquad(squadToEdit){
+            this.editSquad = !this.editSquad;
+            this.squadStoreT.update(squadToEdit)
+        },
+        deleteThisSquad(squadToDelete){
+            this.deleteSquad = !this.deleteSquad;
+            this.squadStoreT.delete(squadToDelete.id)
+        }
+    }
 }
 </script>
