99fde31f56ea2ff869d97a1fe4662ea6e19f0ad0
António Camarão <camaraoacsn@gmail.com>
2023-04-28 09:36:51 +0100

    Corrected name InitialPage component to Dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import InitialPage from "../components/InitialPage.vue"
+import Dashboard from "../components/Dashboard.vue"
 import ListUsers from "../components/ListUsers.vue"
 import ListSquads from "../components/ListSquads.vue"
 import CreateSquad from "../components/CreateSquad.vue"
@@ -8,7 +8,7 @@ import Login from "../components/Login.vue"
 
 const routes = [
     { path: '/', name: 'Login', component: Login },
-    { path: '/dashboard', name: 'Dashboard', component: InitialPage },
+    { path: '/dashboard', name: 'Dashboard', component: Dashboard },
     { path: '/userslist', name: 'ListUsers', component: ListUsers },
     { path: '/squadslist', name: 'ListSquads', component: ListSquads },
     { path: '/squadcreate', name: 'CreateSquad', component: CreateSquad },
