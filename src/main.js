ac674eab5bb3adf96ceb071f1a257739aee5542a
António Camarão <camaraoacsn@gmail.com>
2023-05-04 14:32:30 +0100

    Installation of Vue Toastification in the App

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+const toastOptions = {
+    transition: "Vue-Toastification__fade",
+    maxToasts: 20,
+    newestOnTop: true
+}
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(router)
 app.use(pinia)
+app.use(Toast, toastOptions);
 app.mount('#app')
