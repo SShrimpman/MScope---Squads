4c8fc4cc14da5cc7a80e7c80193fc4d3cf157060
António Camarão <camaraoacsn@gmail.com>
2023-05-04 14:33:27 +0100

    Toasts added to both stores

--- src/stores/squadStore.js
+++ src/stores/squadStore.js
@@ -1,6 +1,23 @@
 import { v4 as uuidv4 } from 'uuid'
 import { defineStore } from 'pinia'
 import { useStorage } from '@vueuse/core'
+import { useToast } from 'vue-toastification'
+
+const toast = useToast()
+const toastCSS = {
+    position: "top-right",
+    timeout: 2500,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: false,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: false,
+    hideProgressBar: true,
+    closeButton: "button",
+    icon: true,
+    rtl: false
+}
 
 export const squadStore = defineStore('squadStore', {
     state: () => {
@@ -16,6 +33,7 @@ export const squadStore = defineStore('squadStore', {
             try {
                 squad.id = uuidv4();
                 this.squads.push(squad);
+                toast.success('Squad Created Successfully!', toastCSS);
             } catch (error) {
                 throw error
             }
@@ -23,6 +41,7 @@ export const squadStore = defineStore('squadStore', {
         update(squad) {
             try {
                 this.squads[this.squads.findIndex(squadFind => squadFind.id == squad.id)] = squad
+                toast.success('Squad Updated Successfully!', toastCSS);
             } catch (error) {
                 throw error
             }
@@ -36,12 +55,14 @@ export const squadStore = defineStore('squadStore', {
 
                 if ( loggedUser == 'TeamLeader' ) {
                     if ( admins.length > 0 ) {
-                        console.log( 'Delete Error : Cant Delete a Squad with a Admin in it!' )
+                        toast.error( "Can't Delete a Squad with a Admin in it!", toastCSS )
                     } else {
                         this.squads.splice( squadToDeleteIndex, 1 )
+                        toast.success('Squad Deleted Successfully!', toastCSS);
                     }
                 } else if( loggedUser == 'Admin' ){
                     this.squads.splice( squadToDeleteIndex, 1 )
+                    toast.success('Squad Deleted Successfully!', toastCSS);
                 }
             } catch (error) {
                 throw error
--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,6 +1,23 @@
 import { v4 as uuidv4 } from 'uuid'
 import { defineStore } from 'pinia'
 import { useStorage } from '@vueuse/core'
+import { useToast } from 'vue-toastification'
+
+const toast = useToast()
+const toastCSS = {
+    position: "top-right",
+    timeout: 2000,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: false,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: false,
+    hideProgressBar: true,
+    closeButton: "button",
+    icon: true,
+    rtl: false
+}
 
 export const userStore = defineStore('userStore', {
     state: () => {
@@ -12,14 +29,14 @@ export const userStore = defineStore('userStore', {
                 { id: uuidv4(), fullName: 'Anthony Shrimp', role: 'Member' }
             ]),
             user: null,
-            isUserLoggedIn : null
+            isUserLoggedIn: null
         }
     },
     getters: {
         getUsers: (state) => state.users,
     },
     actions: {
-        login(user){
+        login(user) {
             if (this.user = user) {
                 return this.isUserLoggedIn = true
             } else {
@@ -30,6 +47,7 @@ export const userStore = defineStore('userStore', {
             try {
                 user.id = uuidv4();
                 this.users.push(user);
+                toast.success('User Created Successfully!', toastCSS);
             } catch (error) {
                 throw error
             }
@@ -40,17 +58,20 @@ export const userStore = defineStore('userStore', {
                 const admins = this.users.filter(user => user.role === 'Admin')
 
                 if (userToUpdate.role != user.role) {
-                    if ( userToUpdate.role === 'Admin' ) {
+                    if (userToUpdate.role === 'Admin') {
                         if (admins.length > 1) {
                             this.users[this.users.findIndex(userFind => userFind.id == user.id)] = user
+                            toast.success('User Updated Successfully!', toastCSS);
                         } else {
-                            console.log('Need to have at least one admin in the App!')
+                            toast.error('Need to have at least one admin in the App!', toastCSS);
                         }
                     } else {
                         this.users[this.users.findIndex(userFind => userFind.id == user.id)] = user
+                        toast.success('User Updated Successfully!', toastCSS);
                     }
                 } else {
                     this.users[this.users.findIndex(userFind => userFind.id == user.id)] = user
+                    toast.success('User Updated Successfully!', toastCSS);
                 }
             } catch (error) {
                 throw error
@@ -61,7 +82,12 @@ export const userStore = defineStore('userStore', {
                 const userToDelete = this.users.map(user => user.id).indexOf(user)
                 const admins = this.users.filter(user => user.role === 'Admin')
 
-                admins.length > 0 ? this.users.splice( userToDelete, 1 ) : console.log('Need to have at least one admin in the App!')
+                if (admins.length <= 1 && this.users[userToDelete].role === 'Admin') {
+                    toast.error('Need to have at least one admin in the App!', toastCSS);
+                } else {
+                    this.users.splice(userToDelete, 1);
+                    toast.success('User Deleted Successfully!', toastCSS);
+                }
             } catch (error) {
                 throw error
             }
