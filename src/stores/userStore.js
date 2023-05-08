2ae123b08b01073d3e68f72c3e7ac6694c8b1f9d
António Camarão <camaraoacsn@gmail.com>
2023-05-08 14:15:57 +0100

    Delete now verifies if user belongs to a squad

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from 'uuid'
 import { defineStore } from 'pinia'
 import { useStorage } from '@vueuse/core'
 import { useToast } from 'vue-toastification'
+import { squadStore } from './squadStore'
 
 const toast = useToast()
 const toastCSS = {
@@ -26,7 +27,10 @@ export const userStore = defineStore('userStore', {
                 // Pre-created user data
                 { id: uuidv4(), fullName: 'Francis Kings', role: 'Admin' },
                 { id: uuidv4(), fullName: 'John Stream', role: 'TeamLeader' },
-                { id: uuidv4(), fullName: 'Anthony Shrimp', role: 'Member' }
+                { id: uuidv4(), fullName: 'Anthony Shrimp', role: 'Member' },
+                { id: uuidv4(), fullName: 'Nathaniel Days', role: 'Member' },
+                { id: uuidv4(), fullName: 'Richard Scallop', role: 'Member' },
+                { id: uuidv4(), fullName: 'John Fun', role: 'Member' }
             ]),
             user: null,
             isUserLoggedIn: null
@@ -61,14 +65,35 @@ export const userStore = defineStore('userStore', {
         },
         delete(user) {
             try {
-                const userToDelete = this.users.map(user => user.id).indexOf(user)
+                const userToDelete = this.users.map(user => user.id).indexOf(user.id)
                 const admins = this.users.filter(user => user.role === 'Admin')
+                const squads = squadStore().getSquads
+                const membersArray = []
+                let squadCount = 0
 
-                if (admins.length <= 1 && this.users[userToDelete].role === 'Admin') {
-                    toast.error('Need to have at least one admin in the App!', toastCSS);
+                for (let i = 0; i < squads.length; i++) {
+                    const squad = squads[i];
+
+                    for (let j = 0; j < squad.members.length; j++) {
+                        membersArray.push(squad.members[j])
+                    }
+                }
+
+                for (let i = 0; i < membersArray.length; i++) {
+                    if (membersArray[i].fullName === user.fullName) {
+                        squadCount++
+                    }
+                }
+
+                if (squadCount > 0) {
+                    toast.error('User is a member of a squad and cannot be deleted!', toastCSS);
                 } else {
-                    this.users.splice(userToDelete, 1);
-                    toast.success('User Deleted Successfully!', toastCSS);
+                    if (admins.length <= 1 && this.users[userToDelete].role === 'Admin') {
+                        toast.error('Need to have at least one admin in the App!', toastCSS);
+                    } else {
+                        this.users.splice(userToDelete, 1);
+                        toast.success('User Deleted Successfully!', toastCSS);
+                    }
                 }
             } catch (error) {
                 throw error
